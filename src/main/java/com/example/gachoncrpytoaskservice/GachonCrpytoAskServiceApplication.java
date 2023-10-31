30652e26de35a5074abf647ac51d3acf1b176135
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-01 02:26:09 +0900

    [init] project 생성

---
+++ src/main/java/com/example/gachoncrpytoaskservice/GachonCrpytoAskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytoaskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachonCrpytoAskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachonCrpytoAskServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gachoncrpytoaskservice/GachonCrpytoAskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytoaskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachonCrpytoAskServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
